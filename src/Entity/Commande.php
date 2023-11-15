1922f461f128e9497f03a48975cef51767ab3ac9
chadha <shadhanejmaoui@gmail.com>
2023-11-15 23:07:12 +0100

    first commit

--- src/Entity/Commande.php
+++ src/Entity/Commande.php
@@ -3,75 +3,138 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-
-/**
- * Commande
- *
- * @ORM\Table(name="commande", indexes={@ORM\Index(name="FK_Commande_Panier", columns={"panierId"})})
- * @ORM\Entity
- */
+use Doctrine\DBAL\Types\Types;
+use App\Repository\CommandeRepository ;
+#[ORM\Entity(repositoryClass: CommandeRepository::class)]
 class Commande
 {
-    /**
-     * @var int
-     *
-     * @ORM\Column(name="commande_id", type="integer", nullable=false)
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="IDENTITY")
-     */
-    private $commandeId;
+    
 
-    /**
-     * @var int
-     *
-     * @ORM\Column(name="client_id", type="integer", nullable=false)
-     */
-    private $clientId;
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: "integer")]
+    private ?int $commandeId = null;
 
-    /**
-     * @var \DateTime|null
-     *
-     * @ORM\Column(name="date_commande", type="date", nullable=true)
-     */
-    private $dateCommande;
+   
+    #[ORM\Column(type: "integer")]
+    private ?int $clientId = null;
 
-    /**
-     * @var float
-     *
-     * @ORM\Column(name="montant_total", type="float", precision=10, scale=0, nullable=false)
-     */
-    private $montantTotal;
 
-    /**
-     * @var string|null
-     *
-     * @ORM\Column(name="adresse_livraison", type="string", length=255, nullable=true)
-     */
-    private $adresseLivraison;
+   
 
-    /**
-     * @var \DateTime|null
-     *
-     * @ORM\Column(name="date_livraison", type="date", nullable=true)
-     */
-    private $dateLivraison;
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTime $dateCommande = null;
 
-    /**
-     * @var string|null
-     *
-     * @ORM\Column(name="mode_paiement", type="string", length=50, nullable=true)
-     */
-    private $modePaiement;
+   
 
-    /**
-     * @var \Panier
-     *
-     * @ORM\ManyToOne(targetEntity="Panier")
-     * @ORM\JoinColumns({
-     *   @ORM\JoinColumn(name="panierId", referencedColumnName="panierId")
-     * })
-     */
-    private $panierid;
+    #[ORM\Column]
+    private ?float $montantTotal = null ;
+
+    
+
+    #[ORM\Column(length: 500)]
+    private ?string $adresseLivraison = null;
+
+   
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTime $dateLivraison = null;
+
+    
+    #[ORM\Column(length: 500)]
+    private ?string $modePaiement = null;
+
+   
+    #[ORM\Column(type: "integer")]
+    private ?int $panierid = null;
+
+    public function getCommandeId(): ?int
+    {
+        return $this->commandeId;
+    }
+
+    public function getClientId(): ?int
+    {
+        return $this->clientId;
+    }
+
+    public function setClientId(int $clientId): static
+    {
+        $this->clientId = $clientId;
+
+        return $this;
+    }
+
+    public function getDateCommande(): ?\DateTimeInterface
+    {
+        return $this->dateCommande;
+    }
+
+    public function setDateCommande(\DateTimeInterface $dateCommande): static
+    {
+        $this->dateCommande = $dateCommande;
+
+        return $this;
+    }
+
+    public function getMontantTotal(): ?float
+    {
+        return $this->montantTotal;
+    }
+
+    public function setMontantTotal(float $montantTotal): static
+    {
+        $this->montantTotal = $montantTotal;
+
+        return $this;
+    }
+
+    public function getAdresseLivraison(): ?string
+    {
+        return $this->adresseLivraison;
+    }
+
+    public function setAdresseLivraison(string $adresseLivraison): static
+    {
+        $this->adresseLivraison = $adresseLivraison;
+
+        return $this;
+    }
+
+    public function getDateLivraison(): ?\DateTimeInterface
+    {
+        return $this->dateLivraison;
+    }
+
+    public function setDateLivraison(\DateTimeInterface $dateLivraison): static
+    {
+        $this->dateLivraison = $dateLivraison;
+
+        return $this;
+    }
+
+    public function getModePaiement(): ?string
+    {
+        return $this->modePaiement;
+    }
+
+    public function setModePaiement(string $modePaiement): static
+    {
+        $this->modePaiement = $modePaiement;
+
+        return $this;
+    }
+
+    public function getPanierid(): ?int
+    {
+        return $this->panierid;
+    }
+
+    public function setPanierid(int $panierid): static
+    {
+        $this->panierid = $panierid;
+
+        return $this;
+    }
 
 
 }
